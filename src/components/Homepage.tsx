8a2db59e694c61ad3bbd618676e708a72465ddb1
kavyaMVG <kavya.mvg@gmail.com>
2023-09-16 13:12:56 +0530

    Add tailwind and homepage design WIP

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,54 +1,36 @@
-import { Link } from "react-router-dom";
-import pat from "../assets/Homepage-bg-cover.jpg";
+import { useState } from "react";
 import "../styles/homepage.css";
 
 export default function Homepage() {
+  const [accountType, setAccountType] = useState<string>("");
   if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
     throw new Error("Missing Publishable Key");
   }
 
   return (
     <div className="homepage">
-      <div className="backk">
-        <center>
-          <div className="container">
-            <div className="flexx">
-              <div style={{ width: "30rem", height: "30rem" }}>
-                <div>
-                  <img
-                    src={pat}
-                    width="400px"
-                    alt="patient"
-                    height="400px"
-                  ></img>
-                  <div>
-                    <button type="button" className="rand">
-                      <Link to="/patient/login">Patient Login </Link>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div
-                style={{ width: "30rem", height: "30rem", marginLeft: "50px" }}
-              >
-                <div>
-                  <img
-                    src={pat}
-                    width="400px"
-                    alt="patient"
-                    height="400px"
-                  ></img>
-                  <div>
-                    <button type="button" className="rand">
-                      <Link to="/doctor/login">Doctor Login </Link>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </center>{" "}
+      <h3 className="text-3xl text-blue-500 text-center font-bold">
+        Choose account type
+      </h3>
+      <div className="card-section">
+        <div
+          className="card card__doctor"
+          onClick={() => setAccountType("Doctor")}
+        >
+          Doctor
+        </div>
+        <div
+          className="card card__patient"
+          onClick={() => setAccountType("Patient")}
+        >
+          Patient
+        </div>
       </div>
+      {accountType && (
+        <button className="rounded-none bg-sky-500">
+          Login as {accountType}
+        </button>
+      )}
     </div>
   );
 }
