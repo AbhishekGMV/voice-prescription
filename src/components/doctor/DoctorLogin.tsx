373952ccf0b2cda03d3be1b895ee55b4dd3703c5
AbhishekGMV <abhishek.mvg@gmail.com>
2023-09-19 21:57:31 +0530

    signature flow

--- src/components/doctor/DoctorLogin.tsx
+++ src/components/doctor/DoctorLogin.tsx
@@ -1,11 +1,13 @@
-import { UserButton, useClerk, useUser } from "@clerk/clerk-react";
+import { useClerk, useUser } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../api";
+import axios from "axios";
 
 function DoctorLogin() {
   const { user, isLoaded } = useUser();
   const [signInComplete, setSignInComplete] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
   const clerk = useClerk();
   const navigate = useNavigate();
 
@@ -23,7 +25,43 @@ function DoctorLogin() {
     }
   }, [user, clerk, isLoaded, navigate]);
 
-  return <div>{user && !signInComplete ? <UserButton /> : ""}</div>;
+  const handleFileUpload = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("sign-image", file);
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+
+    axios.post("/doctor/register", formData, config).then((response) => {
+      console.log(response.data);
+    });
+  };
+
+  return (
+    <div>
+      {user && !signInComplete ? (
+        <>
+          <form onSubmit={(e) => handleFileUpload(e)}>
+            <input
+              type="file"
+              onChange={(e) =>
+                e.target.files && setFile(e.target.files[0] || null)
+              }
+            />
+            <button type="submit">submit</button>
+          </form>
+        </>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default DoctorLogin;
