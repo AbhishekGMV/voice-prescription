af9559099851bb4025e306780671fa80de72c4f8
AbhishekGMV <abhishek.mvg@gmail.com>
2023-09-19 14:18:10 +0530

    signature flow

--- src/components/doctor/DoctorLogin.tsx
+++ src/components/doctor/DoctorLogin.tsx
@@ -1,23 +1,29 @@
 import { UserButton, useClerk, useUser } from "@clerk/clerk-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import api from "../../api";
 
 function DoctorLogin() {
   const { user, isLoaded } = useUser();
+  const [signInComplete, setSignInComplete] = useState(false);
   const clerk = useClerk();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const checkIfUserSignedIn = async () => {
-      if (isLoaded && !user) {
-        await clerk.redirectToSignIn();
-      }
-    };
-    checkIfUserSignedIn();
-    if (isLoaded && user) navigate("/doctor/dashboard");
+    if (isLoaded && !user) clerk.redirectToSignIn();
+
+    if (isLoaded && user) {
+      api.get(`/doctor/${user.id}`).then(({ data }) => {
+        const signature = data.doctor.signatureUrl;
+        if (signature) {
+          setSignInComplete(true);
+          navigate("/doctor/dashboard");
+        }
+      });
+    }
   }, [user, clerk, isLoaded, navigate]);
 
-  return <div>{user && <UserButton />}</div>;
+  return <div>{user && !signInComplete ? <UserButton /> : ""}</div>;
 }
 
 export default DoctorLogin;
