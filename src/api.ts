414b5a7b0cd5cf41700872f40e28dc29089ab18c
AbhishekGMV <abhishek.mvg@gmail.com>
2024-02-26 16:52:34 +0530

    merge main

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+console.log(import.meta.env.VITE_API_PROD_BASE_URL);
 export default axios.create({
   baseURL:
     import.meta.env.VITE_API_PROD_BASE_URL || "http://localhost:5002/",
