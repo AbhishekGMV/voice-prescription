80439790c9176e94b0b5be4731a9009d4293b2c4
abhishekgmv <abhishek.mvg@gmail.com>
2024-03-25 20:40:35 +0530

    update header nav

--- src/components/doctor/MainNav.tsx
+++ src/components/doctor/MainNav.tsx
@@ -1,28 +1,39 @@
 import { cn } from "@/lib/utils";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const location = useLocation();
+  const routes = [
+    {
+      path: "/doctor/dashboard",
+      section: "Dashboard",
+      className: "text-sm font-medium transition-colors hover:text-primary",
+    },
+    {
+      path: "/doctor/appointment",
+      section: "Appointment",
+      className: "text-sm font-medium transition-colors hover:text-primary",
+    },
+  ];
   return (
     <nav
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
       {...props}
     >
-      <Link
-        to="/doctor/dashboard"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        Dashboard
-      </Link>
-      <Link
-        to="/doctor/appointment"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Appointments
-      </Link>
-      
+      {routes.map((route, idx) => (
+        <Link
+          to={route.path}
+          key={idx}
+          className={`${route.className} ${
+            route.path !== location.pathname ? "text-muted-foreground" : ""
+          }  `}
+        >
+          {route.section}
+        </Link>
+      ))}
     </nav>
   );
 }
